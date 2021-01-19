bd5c87eace43fa5e52f0a8e36665d42a624a9ec1
bardovasco <bardovasco@protonmail.com>
2021-01-18 23:24:53 -0600

    signIn signUp POST

--- backend-service/resources/user_resource.py
+++ backend-service/resources/user_resource.py
@@ -1,4 +1,6 @@
+from hashlib import md5
 from datetime import datetime
+from flask import jsonify, abort
 from flask_restful import Resource
 from app import db
 from services.user_middleware import UserMiddleware, UserSchema
@@ -11,34 +13,51 @@ class UserResrouce(Resource):
     User API
     """
     middleware = UserMiddleware()
+    def post(self, service):
+        if service == 'signUp':
+            return self.user_signup_service()
+        elif service == 'signIn':
+            return self.user_signin_service()
 
-    def post(self):
+    def user_signup_service(self):
         user_data = self.middleware.read_post()
         user_obj = User(
             username=user_data['username'],
             email=user_data['email'],
-            passphrase=user_data['passphrase'],
+            passphrase=md5(
+                # encrypt user passphrase
+                user_data['passphrase']\
+                .encode('utf-8')).hexdigest(),
             created_at=datetime.now(),
             updated_at=datetime.now())
 
         # Call QueryService and bind User model as resource
         user_service = ResourceQueryService(db.session, user_obj)
-        user_service.create_one(user_obj)
-        return {}, 201
+        result_data = user_service.create_one(user_obj)
+        if not result_data:
+            return {}, 201
+        else:
+            return abort(404, result_data)
 
-    def get(self):
+    def user_signin_service(self):
         user_data = self.middleware.read_get()
 
         # Call QueryService passing "email" arg
         user_service = ResourceQueryService(db.session, User)
         result = user_service.get_by_email(user_data['email'])
-        user_dump = self.middleware.schema.dump(result)
-        return user_dump, 200
+
+        user_data['passphrase'] = md5(user_data['passphrase'].encode('utf-8'))
+        if result and user_data['passphrase'].hexdigest() == result.passphrase:
+            # Serialize object
+            user_dump = self.middleware.schema.dump(result)
+            return user_dump, 200
+        else:
+            return abort(401, 'authentication error')
 
     def put(self):
         user_data = self.middleware.read_update()
         # Call QueryService and bind User model as resource
-        user_service = ResourceQueryService(db.session, user_data)
+        # user_service = ResourceQueryService(db.session, user_data)
         return {}, 200
 
     def delete(self):
