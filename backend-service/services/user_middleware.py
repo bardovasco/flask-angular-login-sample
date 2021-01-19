d9cf4dc413332a428c48947edff2444005059d8d
bardovasco <bardovasco@protonmail.com>
2021-01-18 23:25:17 -0600

    post middleware

--- backend-service/services/user_middleware.py
+++ backend-service/services/user_middleware.py
@@ -6,37 +6,20 @@ Explaination of dual data parser:
     snakecase format with Model objects.
 """
 from flask_restful.reqparse import RequestParser
-from marshmallow import Schema, fields, post_dump
-
+from marshmallow import Schema, fields, post_dump, post_load
 
 
 class UserSchema(Schema):
     """
-    User Schema to dump and load
-    our user objects.
+    Schema to load and dump
+    user objects.
     """
-    # Default fields
     id = fields.Integer(attribute='id')
     username = fields.String(attribute='username')
     email = fields.String(attribute='email')
-    passPhrase = fields.String(attribute='passphrase', load_only=True)
     createdAt = fields.String(attribute='created_at')
     updatedAt = fields.String(attribute='updated_at')
-    # Fields for PUT method
-    newEmail = fields.String(attribute='new_email', load_only=True)
-    newUsername = fields.String(attribute='new_username', load_only=True)
-    newPassPhrase = fields.String(attribute='new_passphrase', load_only=True)
-
-    @post_dump
-    def delete_null_values(self, data, *args, **kwargs):
-        """
-        PreDump function to clear
-        unnecessary fields.
-        """
-        for k,v in data.items():
-            if not v:
-                del data[k]
-        return data
+    passPhrase = fields.String(attribute='passphrase', load_only=True)
 
 class UserMiddleware(RequestParser):
     """
@@ -48,8 +31,6 @@ class UserMiddleware(RequestParser):
         RequestParser.__init__(self)
 
     def read_post(self):
-        self.clear_args()
-
         # Parse args
         self.add_argument('username', type=str, location=('json'), required=True, case_sensitive=True)
         self.add_argument('email', type=str, location=('json'), required=True, case_sensitive=True)
@@ -57,47 +38,19 @@ class UserMiddleware(RequestParser):
 
         # Read args
         data = self.parse_args(strict=True)
-        # Clear current args
-        self.clear_args()
-
+        self.remove_argument('username')
         # Schema load
         loaded_data = self.schema.load(data)
         return loaded_data
 
     def read_get(self):
-        self.clear_args()
-
         # Parse args
         self.add_argument('email', type=str, location=('json'), required=True, case_sensitive=True)
         self.add_argument('passPhrase', type=str, location=('json'), required=True, case_sensitive=True)
 
         # Read args
         data = self.parse_args(strict=True)
-        # Clear current args
-        self.clear_args()
-
         # Schema load
         loaded_data = self.schema.load(data)
         return loaded_data
-
-    def read_update(self):
-        self.clear_args()
-
-        # Parse args
-        self.add_argument('id', type=int, location=('json'), required=True)
-        self.add_argument('newEmail', type=str, location=('json'), required=False)
-        self.add_argument('newUsername', type=str, location=('json'), required=False)
-        self.add_argument('newPassPhrase', type=str, location=('json'), required=False)
-        # Read args
-        data = self.parse_args(strict=True)
-        # Clear current args
-        self.clear_args()
-
-        # Schema load
-        loaded_data = self.schema.load(data)
-        return loaded_data
-
-    def clear_args(self) -> None:
-        for arg in self.args:
-            self.remove_argument(arg.name)
 
