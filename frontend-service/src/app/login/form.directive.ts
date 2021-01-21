8148ab1d94f5db65796195de52a85899559f20cc
bardovasco <bardovasco@protonmail.com>
2021-01-20 23:57:17 -0600

    login directive

---
+++ frontend-service/src/app/login/form.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, HostListener, ElementRef } from '@angular/core';
+
+
+@Directive({
+  selector: '[focusInvalidInput]'
+})
+export class FormDirective {
+
+  constructor(
+    private el: ElementRef
+  ) {}
+
+    @HostListener('submit')
+    onSubmit() {
+      const invalidControl = this.el.nativeElement.querySelector('.ng-invalid')
+
+      if (invalidControl) {
+        invalidControl.focus()
+      }
+    }
+}
