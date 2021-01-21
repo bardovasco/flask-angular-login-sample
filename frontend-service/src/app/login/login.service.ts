ec870c0b1075fdeeb691aef66250c41ff50eb2e8
bardovasco <bardovasco@protonmail.com>
2021-01-20 23:56:17 -0600

    login service

--- frontend-service/src/app/login/login.service.ts
+++ frontend-service/src/app/login/login.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export class User {
+  id?: number;
+  username: string;
+  email: string;
+  passPhrase: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  //userApi = 'http://127.0.0.1:5000/user';
+  userApi = 'https://enfvseeir93s3xr.m.pipedream.net';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  httpHeaders = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  addUser(userData): Observable<User> {
+    return this.httpClient.post<User>(
+      this.userApi,
+      JSON.stringify(userData),
+      this.httpHeaders
+    ).pipe(
+      retry(1),
+      catchError(this.processError)
+    )
+  }
+
+  processError(err) {
+    let message = '';
+    if (err.error instanceof ErrorEvent) {
+      message = err.error.message;
+    }
+    else {
+      message = `Error code: ${err.status}\nMessage: ${err.message}`
+    }
+    console.log(message);
+    return throwError(message);
+  }
 }
