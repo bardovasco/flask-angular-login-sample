587132b02a8b12dcbb5a0f48d69343dd5d579174
bardovasco <bardovasco@protonmail.com>
2021-01-20 23:58:10 -0600

    signup component and addUser

--- frontend-service/src/app/login/sign-up/sign-up.component.ts
+++ frontend-service/src/app/login/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { LoginService, User } from '../login.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
+  @Input() userData: User = {
+    username: '',
+    email: '',
+    passPhrase: ''
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService
+  ) {
+    this.userForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      passPhrase: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    return this.loginService.addUser(this.userData)
+      .subscribe(res => console.log(res))
+  }
 }
