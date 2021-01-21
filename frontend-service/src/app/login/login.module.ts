59604924cde62924fa4f7cb08be5d4d8ce18433a
bardovasco <bardovasco@protonmail.com>
2021-01-20 23:56:57 -0600

    input validation directive

--- frontend-service/src/app/login/login.module.ts
+++ frontend-service/src/app/login/login.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginDashboardComponent } from './login-dashboard/login-dashboard.component';
+import { FormDirective } from './form.directive';
 
 
 @NgModule({
   declarations: [
     SignInComponent,
     SignUpComponent,
-    LoginDashboardComponent
+    LoginDashboardComponent,
+    FormDirective
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class LoginModule { }
