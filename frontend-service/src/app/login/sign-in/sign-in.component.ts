050d21f6a2689ee1a03c6985144ca12920ed13eb
bardovasco <bardovasco@protonmail.com>
2021-01-23 23:33:14 -0600

    sign in component

--- frontend-service/src/app/login/sign-in/sign-in.component.ts
+++ frontend-service/src/app/login/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Md5 } from 'ts-md5/dist/md5';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.userForm = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required]],
+      passPhrase: ['', [Validators.required]]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(userData) {
+    userData.passPhrase = Md5.hashStr(userData.passPhrase);
+    console.log(userData);
+  }
 }
